7a2b1feb9eee4f7e5e5719bbe75f64c665255e1d
Jimmy Lim <jimmy@Jimmys-Box-for-Cats.local>
2019-10-23 21:00:13 -0700

    proxy working

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.json());
-app.use('/:propertyID/propertyID', express.static(path.join(__dirname, './public')));
+app.use('/:propertyId', express.static(__dirname));
 
-app.listen(3002);
+app.listen(3100);
